2fddfdb6d17eaf4ca5abda776c8e5c9098014d5f
Chance Strickland <chance.strickland@gmail.com>
2018-12-27 13:24:28 -0600

    update build script

--- src/server/controllers/forms.js
+++ src/server/controllers/forms.js
@@ -3,7 +3,6 @@ const handleDrip = require('../services/drip');
 const router = express.Router();
 
 router.post('/drip', (req, res, next) => {
-  console.log({ body: req.body });
   handleDrip(req.body)
     .then((response) => res.status(200).json(response))
     .catch((err) => next(err || {}));
